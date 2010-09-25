905502b0ae35ef16104e003ead0e204f11bd1bc2
Alberto Fernández Capel <afcapel@gmail.com>
2010-09-25 20:20:39 +0200

    Access table columns by index

--- lib/table.rb
+++ lib/table.rb
@@ -7,9 +7,25 @@ class Table
     @columns = columns.collect { |name| name.downcase}
   end
   
-  def column(name)
-    index = @columns.index(name.downcase)
-    column_at index
+  def columns
+    columns = []
+    row_length = rows.empty? ? 0 : rows[0].size
+    
+    0.upto(row_length).each do |i|
+      columns << rows.collect { |row| row[i] }
+    end
+    
+    columns
+  end
+  
+  def column(col)
+    case col
+    when String
+      index = @columns.index(col.downcase)
+      column_at index
+    when Integer
+      column_at col
+    end
   end
   
   private
--- spec/scenarios/table_spec.rb
+++ spec/scenarios/table_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe Table do
+  
+  before do
+    books = [["Don Quixote", "Miguel de Cervantes"], ["War and Peace", "Leo Tolstoy"]]
+    @table = Table.new books, :columns => ['Book', 'Author']
+  end
+  
   it "should be possible to create a new table with a two dimensional array" do
     table = Table.new [["Don Quixote", "Miguel de Cervantes"], ["War and Peace", "Leo Tolstoy"]]
+    
     table.rows[0].should == ["Don Quixote", "Miguel de Cervantes"]
     table.rows[1].should == ["War and Peace", "Leo Tolstoy"]
   end
@@ -22,11 +29,18 @@ describe Table do
   end
   
   it "should be possible to set column names" do
-    books = [["Don Quixote", "Miguel de Cervantes"], ["War and Peace", "Leo Tolstoy"]]
-    table = Table.new books, :columns => ['Book', 'Author']
     
-    table.column('author').should == ["Miguel de Cervantes", "Leo Tolstoy"]
-    table.column('book').should == ["Don Quixote", "War and Peace"]
+    @table.column('book').should == ["Don Quixote", "War and Peace"]
+    @table.column('author').should == ["Miguel de Cervantes", "Leo Tolstoy"]
+  end
+  
+  it "should be possible to refer to the column by its zero-based ordinal position" do
+    
+    @table.column(0).should == ["Don Quixote", "War and Peace"]
+    @table.column(1).should == ["Miguel de Cervantes", "Leo Tolstoy"]
+    
+    @table.columns[0].should == ["Don Quixote", "War and Peace"]
+    @table.columns[1].should == ["Miguel de Cervantes", "Leo Tolstoy"]
   end
   
 end
