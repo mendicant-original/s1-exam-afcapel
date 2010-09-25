9b46da4079e98fe40edbef68f4520814bb58eee8
Alberto Fernández Capel <afcapel@gmail.com>
2010-09-25 22:08:22 +0200

    Change column name

--- lib/table.rb
+++ lib/table.rb
@@ -1,27 +1,40 @@
+class Column < Array
+  attr_accessor :name
+end
+
 class Table
   attr_reader :rows
   
   def initialize(array = [], options = {})
     @rows = array
-    columns = options[:columns] || []
-    @columns = columns.collect { |name| name.downcase}
+    @columns = []
+    
+    if options[:columns]
+      options[:columns].each do |name|
+        column = Column.new
+        column.name = name
+        @columns << column
+      end
+    else
+      0.upto(row_length).each do |i|
+        @columns << Column.new
+      end
+    end
   end
   
   def columns
-    columns = []
-    row_length = rows.empty? ? 0 : rows[0].size
-    
-    0.upto(row_length).each do |i|
-      columns << rows.collect { |row| row[i] }
+    @columns.each_index do |i|
+      @columns[i] = column_at(i)
     end
     
-    columns
+    @columns
   end
   
   def column(col)
     case col
     when String
-      index = @columns.index(col.downcase)
+      column =  @columns.find { |c| c.name.downcase == col.downcase }
+      index = @columns.index column
       column_at index
     when Integer
       column_at col
@@ -34,8 +47,18 @@ class Table
   
   private
   
+  def row_length
+    rows.empty? ? 0 : rows[0].size
+  end
+  
   def column_at(index)
-    rows.collect{ |row| row[index]}
+    @columns[index].clear
+    
+    @rows.each_index do |i|
+      @columns[index] << @rows[i][index]
+    end
+    
+    @columns[index]
   end
   
 end
--- spec/scenarios/column_manipulation_spec.rb
+++ spec/scenarios/column_manipulation_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Table, "Column manipulation" do
+describe Table, "column manipulation" do
   
   before do
     books = [["Don Quixote", "Miguel de Cervantes"], ["War and Peace", "Leo Tolstoy"]]
@@ -10,6 +10,14 @@ describe Table, "Column manipulation" do
   it "should be possible to retrieve an array consisting of all the contents in that column" do
     @table.columns[0].should == ["Don Quixote", "War and Peace"]
     @table.columns[1].should == ["Miguel de Cervantes", "Leo Tolstoy"]
+  end
+  
+  it "should be possible rename a column" do
+    @table.column(1).name = "Writer"
+    @table.column('writer').should == ["Miguel de Cervantes", "Leo Tolstoy"]
+    
+    @table.columns[0].name = "Novel"
+    @table.column('novel').should == ["Don Quixote", "War and Peace"]
   end
   
 end
